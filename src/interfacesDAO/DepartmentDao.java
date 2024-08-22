008dea58aa75fc31316312baa6a9a43131fee62a
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-08-22 10:42:53 -0300

    Criacao das interfaces DepartimentDao e SellerDao

---
+++ src/interfacesDAO/DepartmentDao.java
@@ -0,0 +1,13 @@
+package interfacesDAO;
+
+import entities.Department;
+
+public interface DepartmentDao {
+
+    void insert(Department dep);
+    void update(Department dep);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+} 
---
+++ src/interfacesDAO/SellerDao.java
@@ -0,0 +1,19 @@
+package interfacesDAO;
+
+import entities.Department;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Department dep);
+
+    void update(Department dep);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+    
+} 
